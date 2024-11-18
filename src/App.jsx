36d20e9188c84337f24fde0af315f486ed33955d
Huy Vo <vominhhuy0911@gmail.com>
2024-11-18 21:06:06 +0700

    add css baseline

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import './App.css'
 import Button from '@mui/material/Button'
 
 function App() {
-  return <Button variant="contained">Hello world</Button>
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+      <p>minhuy</p>
+    </>
+  )
 }
 
 export default App
