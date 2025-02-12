6951029ee5595b4a97f6dae3ad7195aba132adef
Huy Vo <forwork@minhuy.dev>
2025-02-12 18:28:25 +0700

    build mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,13 @@
-export const API_ROOT = 'http://localhost:8017'
+/* eslint-disable no-undef */
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-28mv.onrender.com'
+}
+
+export const API_ROOT = apiRoot
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
